0b4f012b334b40a18b3395a33d788128e9cb8640
Fajrian Muchtar <fajrianmuchtarr@gmail.com>
2022-01-10 22:40:46 +0700

    Update validate_social_media.js
    revision test case social media

--- cypress/integration/validate_social_media.js
+++ cypress/integration/validate_social_media.js
@@ -1,8 +1,10 @@
 import Login from '../support/PageObjects/LoginPage'
+import SocialMedia from '../support/PageObjects/SocialMedia'
 
 it ('Validate_social_media', function(){
 
 const login=new Login();
+const navbar=new SocialMedia();
 
 cy.visit(Cypress.env('url'))
 
@@ -15,6 +17,18 @@ login.getPasswordBox()
 login.getLoginButton()
 .click()
 
+navbar.getFacebook()
+.click()
+.should('have.attr' , 'href' , 'https://www.facebook.com/saucelabs');
 
+navbar.getLinkedin()
+.click()
+.should('have.attr', 'href', 'https://www.linkedin.com/company/sauce-labs/')
+
+navbar.getTwitter()
+.click()
+.should('have.attr', 'href' , 'https://twitter.com/saucelabs')
 
 })
+
+
